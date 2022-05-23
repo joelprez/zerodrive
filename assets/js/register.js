b26a1a2c523c871aa7a960c35a2b7e8504121efb
joelprez <72136563+joelprez@users.noreply.github.com>
2022-05-23 16:59:26 +0200

    register script update

--- assets/js/register.js
+++ assets/js/register.js
@@ -1,11 +1,15 @@
+let message = document.getElementById("message");
 let email = document.querySelector('[name="email"]');
 let password = document.querySelector('[name="password"]');
 let meter = document.getElementById('password-strength');
 let password2 = document.querySelector(['[name="password2"]']);
+let csrf = document.getElementById("csrf");
 let registerBtn = document.getElementById("register-btn");
 
-// email check regex
-let validEmail = /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/;
+// check if recaptcha is checked
+function isCaptchaChecked() {
+    return grecaptcha && grecaptcha.getResponse().length !== 0;
+}
 
 // hide and show input checks and meter
 email.addEventListener('input', function() {
@@ -17,9 +21,22 @@ email.addEventListener('input', function() {
     }
     
     if (email.value.match(validEmail)) {
-        email.classList.add("input-check-meter-success");
+        $.post("api/emailCheck.php", {
+            email: email.value,
+            csrf: csrf.value,
+        }, function(result) {
+            let output = JSON.parse(result);
+            if (output.success) {
+                email.classList.add("input-check-meter-success");
+                email.valid = true;
+            } else {
+                email.classList.remove("input-check-meter-success");
+                email.valid = false;
+            }
+        });
     } else {
         email.classList.remove("input-check-meter-success");
+        email.valid = false;
     }
 });
 
@@ -44,13 +61,58 @@ password2.addEventListener('input', function() {
 
     if (password.value === password2.value && password2.value != "" && meter.value === 4) {
         password2.classList.add("input-check-meter-success");
+        password2.valid = true;
     } else {
         password2.classList.remove("input-check-meter-success");
-
+        password2.valid = true;
     }
 });
 
 registerBtn.onclick = function() {
-
-    $("#toast").fadeIn("fast", function() { $(this).delay(1000).fadeOut("slow"); });
+    message.classList.remove("hidden");
+    if (email.valid && password2.valid && isCaptchaChecked()) {
+        // add hash loading animation
+        message.classList.add("bg-gray-400");
+        message.innerHTML = 'Hashing your password <div class="lds-facebook"><div></div><div></div><div></div></div>';
+        // wait 2.5 seconds
+        setTimeout(function() {
+            // hash password
+            let hashed_password = hash_password(password.value);
+            // send data to API
+            $.post("api/register.php", {
+                email: email.value,
+                password: hashed_password,
+                g_recaptcha_response: document.getElementById("g-recaptcha-response").value,
+                csrf: csrf.value
+            }, function(result) {
+                let output = JSON.parse(result);
+                if (output.success) {
+                    registerBtn.disabled = true;
+                    message.classList.remove("bg-red-600");
+                    message.classList.remove("bg-gray-400");
+                    message.classList.add("bg-green-600");
+                    message.innerText = "Account created, redirecting in 3 seconds";
+                    setTimeout(function() {
+                        window.location.href = "login.php";
+                    }, 3000);
+                } else {
+                    message.classList.remove("bg-green-600");
+                    message.classList.add("bg-red-600");
+                    message.innerText = output.message;
+                }
+            });
+        }, 2500);
+    } else {
+        message.classList.remove("bg-gray-400");
+        message.classList.remove("bg-green-600");
+        message.classList.add("bg-red-600");
+        if (!email.valid) {
+            message.innerText = "Email is not valid or it's already registered";
+        } else if (!password2.valid) {
+            message.innerText = "Password is not valid";
+        } else if (!isCaptchaChecked()) {
+            message.innerText = "Captcha is missing";
+        }
+        
+    }
 }
