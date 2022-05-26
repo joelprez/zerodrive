a109aa08b5af8d5a509044cb6d161c6da14f72f1
joelprez <72136563+joelprez@users.noreply.github.com>
2022-05-26 20:22:16 +0200

    default config.php

---
+++ inc/config.php
@@ -0,0 +1,13 @@
+<?php
+// database information
+$db_name = "zerodrive";
+$db_username = "root";
+$db_password = "";
+
+// recaptcha keys
+$recaptcha_secret = "";
+$recaptcha_public = "";
+
+// password hash
+$passwordhash_bcrypt_cost = 15;
+?>
