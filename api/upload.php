e4f573b491402092d505c4960c7e8570fdccc750
joelprez <72136563+joelprez@users.noreply.github.com>
2022-05-26 20:15:17 +0200

    Update upload.php

--- api/upload.php
+++ api/upload.php
@@ -1,11 +1,7 @@
 <?php
 session_start();
 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
-if (isset($_POST["filename"], $_POST["mime"], $_POST["aes_private_key"], $_POST["aes_iv"], $_POST["file"])) {
+if (isset($_SESSION["id"], $_POST["filename"], $_POST["mime"], $_POST["aes_private_key"], $_POST["aes_iv"], $_POST["file"])) {
     require "../inc/conn.php";
     $insert = $conn->prepare("INSERT INTO files (name, path, mime, file_aes256cbc_private_key, file_aes256cbc_iv, owner_id) VALUES (?, NULL, ?, ?, ?, ?)");
     $insert->bindParam(1, $_POST["filename"], PDO::PARAM_STR);
@@ -22,5 +18,7 @@ if (isset($_POST["filename"], $_POST["mime"], $_POST["aes_private_key"], $_POST[
 
     file_put_contents("../files/" . $select["id"], $_POST["file"]);
     echo $select["id"];
+} else {
+    echo json_encode(["success" => false]);
 }
 ?>
